7d9de4c5e4434bafdd1fb1c21a5f8acbc1abc96c
cathalmcB <cathalmc1@hotmail.co.uk>
2018-03-31 17:08:15 +0100

    Add files via upload

---
+++ FOOP202Project/Entry.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOOP202Project
+{
+    class Entry
+    {
+        private string name;
+        private string source;
+        private Uri image;
+        private string description;
+        private string[] keywords;
+        protected string ImageType;
+
+        #region Assigning Values
+        public string Name { get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Source
+        {
+            get
+            {
+                return source;
+            }
+            set
+            {
+                source = value;
+            }
+        }
+        public Uri Image
+        {
+            get
+            {
+                return image;
+            }
+            set
+            {
+                image = value;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+        public string[] Keywords
+        {
+            get
+            {
+                return keywords;
+            }
+            set
+            {
+                keywords = value;
+            }
+        }
+        #endregion 
+
+        public Entry(string Name, string Source, string Description)
+        {
+            this.Name = Name;
+            this.Source = Source;
+            this.Description = Description;
+            //this.Keywords = keywords;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+
+        static public string GetImageDirectory()
+        {
+            string imageDirectory = "";
+            string currentDir = Directory.GetCurrentDirectory();
+            DirectoryInfo parent = Directory.GetParent(currentDir);
+            DirectoryInfo grandParent = Directory.GetParent(parent.FullName);
+
+
+
+            imageDirectory = grandParent + "\\Images\\";
+
+
+
+            return imageDirectory;
+        }
+    }
+
+    #region Category Constructors
+    class Networking : Entry
+    {
+        public Networking(string Name, string Source, string Description) : base(Name, Source, Description)
+        {
+           ImageType  = "Networking.png";
+           Image = new Uri(GetImageDirectory() + ImageType, UriKind.Absolute);
+        }
+    }
+
+    class Database : Entry
+    {
+        public Database(string Name, string Source, string Description, string[] Keywords) : base(Name, Source, Description)
+        {
+            ImageType = "Database.png";
+            Image = new Uri(GetImageDirectory() + ImageType, UriKind.Absolute);
+        }
+    }
+    class Internet : Entry
+    {
+        public Internet(string Name, string Source, string Description, string[] Keywords) : base(Name, Source, Description)
+        {
+            ImageType = "Internet.png";
+            Image = new Uri(GetImageDirectory() + ImageType, UriKind.Absolute);
+        }
+    }
+    class Scams : Entry
+    {
+        public Scams(string Name, string Source, string Description, string[] Keywords) : base(Name, Source, Description)
+        {
+            ImageType = "Scams.png";
+            Image = new Uri(GetImageDirectory() + ImageType, UriKind.Absolute);
+        }
+    }
+    #endregion
+    
+}
---
+++ FOOP202Project/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FOOP202Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string[] ComboOptions = { "Networking", "Database", "Internet", "Scams"};
+        ObservableCollection<Entry> EntriesList = new ObservableCollection<Entry>();
+        Networking Test1 = new Networking("Test", "http://www.facebook.com", "Test 1 Description");
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            EntriesList.Add(Test1);
+            LstBxMain.ItemsSource = EntriesList;
+            CmboxFilter.ItemsSource = ComboOptions;
+            CmboxFilter.SelectedIndex = 0;
+        }
+
+        private void LstBxMain_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Entry selectedItem = LstBxMain.SelectedItem as Entry;
+            if (selectedItem != null)
+            {
+                LblName.Content = selectedItem.Name;
+                LblType.Content = selectedItem.GetType();
+                TxtBlckDesc.Text = selectedItem.Description;
+                Img.Source = new BitmapImage(selectedItem.Image);
+
+            }
+        }
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Entry selectedItem = LstBxMain.SelectedItem as Entry;
+            if (selectedItem != null)
+            {
+                System.Diagnostics.Process.Start(selectedItem.Source);
+            }
+        }
+    }
+}
